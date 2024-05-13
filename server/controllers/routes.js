5533bfac4c14128e8264bff10858b47e9935b473
ajflores326 <armandflores326@gmail.com>
2024-05-13 07:43:18 -0400

    announcement post test

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,9 +1,28 @@
 import { Router } from "express";
+import Announcement from "../models/announcements";
 
 const router = Router();
 
 router.get("/", (request,response) => {
     response.send("Server connected")
 })
+
+router.post("/createannouncement", validationMiddleware, async (request, response) => {
+
+    
+    const announcement = new Announcement({
+        title: request.body.room,
+        user: request.body.user,
+        title: request.body.title,
+        body: request.body.body
+        
+    })
+
+    await announcement.save();
+
+    response.send("Announcement posted")
+
+})
+
 
 export default router;

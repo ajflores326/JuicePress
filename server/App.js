1cf0e84e4f151c1cfad160dbe58ab391d3c46dc8
Your Name <130116701+royfrias@users.noreply.github.com>
2024-05-14 15:50:39 -0400

    updates to my env, routes-users file and some other updates to my package.json

--- server/App.js
+++ server/App.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import "dotenv/config";
+import userRouter from "./controllers/routes-user.js";
 import router from "./controllers/routes.js";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -8,6 +9,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/", userRouter);
 app.use("/", router);
 
 app.listen(process.env.SERVER_PORT, () => {
--- server/controllers/routes-user.js
+++ server/controllers/routes-user.js
@@ -2,8 +2,51 @@ import { Router, request } from "express";
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-import validationMiddleware from "../middleware/validationMiddleware.js";
+
 
 const router = Router();
+
+router.post("/signup", async (request, response)=>{
+  // this will check if an email is duplicated
+  try {
+    const doesEmailExist = await User.exists({
+      email: request.body.email
+    });
+
+    if(doesEmailExist) {
+      return response.status(400).json({
+        message: "Email is already in use"
+      });
+    }
+
+    // this hashes password
+    const hashedPassword = await bcryptjs.hash(request.body.password, 10);
+
+    // creates new user 
+    const newUser = new User({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      username: request.body.username,
+      password: hashedPassword,
+      email: request.body.email
+    });
+
+    //saves user to database
+    await newUser.save();
+
+    //generates JWT token
+    const token = jwt.sign({id: newUser._id}, process.env.SECRET_KEY);
+
+    // if a successful response is made
+    response.status(201).json({
+      message: "User Created Successfully",
+      token
+    });
+    } catch(error) {
+      console.error("Error creating user:", error);
+      response.status(500).json({message: "An error occurred, please try again."})
+    }
+});
+
 
 export default router;

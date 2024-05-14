bd1b75afb12ec953bc7527b74af1811c4267b029
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-05-14 16:50:08 -0400

    add juice press logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,48 @@
-import React, { useState } from 'react'
-import './App.css'
+// import React, { useState } from 'react'
+// import './App.css'
 
-
+import "./components/styles/LogIn.css";
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import JPLogo from './images/JPLogo.png';
 
 function App() {
  
 
   return (
-    <div>
-     Juice Press Home Page
-    </div>
+   
+      <div>
+
+        
+       <form id='formbox'>
+       <h2>Let's See What's Juicy!</h2>
+      <input
+        id="exampleEmail"
+        name="email"
+        placeholder="Email"
+        type="email"
+      />
+    
+    {' '}
+    
+      <input
+        id="examplePassword"
+        name="password"
+        placeholder="Password"
+        type="password"
+      />
+    
+    {' '}
+    <button type="sumbit">
+      Log In
+    </button>
+    <p>Don't have an account?</p>
+    <button>Sign Up</button>
+    <img src={JPLogo} alt="Juice Press Logo" width="45%" height="45%"></img>
+    {/* <Link to="/" className="signup">Sign Up</Link> */}
+  </form>
+ 
+      </div>
   )
 }
 
--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -11,33 +11,55 @@ export default function LogIn() {
 
     async function sumbitSignIn(event) {
         event.preventDefault();
+        const sumbit = await fetch("http://localhost:3000/login", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify({
+            email,
+            password
+          })
+        });
+
+        const body = await response.json();
+        if (response.status === 200) {
+          localStorage.setItem("jwt-token", body.token);
+          setToken(body.token);
+          navigate('/home')
+        } else {
+          console.log(body.message)
+        }
     }
     return (
         <div>
         <h2>Let's See What's Juicy!</h2>
-       <form id='formbox'>
-      
-      <input
-        id="exampleEmail"
-        name="email"
-        placeholder="Email"
-        type="email"
-      />
-    
-    {' '}
-    
-      <input
-        id="examplePassword"
-        name="password"
-        placeholder="Password"
-        type="password"
-      />
-    
-    {' '}
-    <button>
-      Log In
-    </button>
-  </form>
-      </div>
+        <form id='formbox'>
+       
+       <input
+         id="exampleEmail"
+         name="email"
+         placeholder="Email"
+         type="email"
+       />
+     
+     {' '}
+     
+       <input
+         id="examplePassword"
+         name="password"
+         placeholder="Password"
+         type="password"
+       />
+     
+     {' '}
+     <button type="sumbit">
+       Log In
+     </button>
+     <p>Don't have an account?</p>
+     <button type="sumbit">Sign Up</button>
+     {/* <Link to="/" className="signup">Sign Up</Link> */}
+   </form>
+       </div>
     )
 }

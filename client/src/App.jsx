8a41282c0e10a850519249bf5ae9eb57146cb9ca
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-05-15 19:23:39 -0400

    small changes to home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import './components/styles/Home.css';
 import JPLogo from './images/JPLogo.png';
 
@@ -9,60 +8,26 @@ function App() {
 
 
   return (
-
-    <div id='container'>
-
-<img src={JPLogo} alt="Juice Press Logo" width="30%" height="30%"></img>
+    
+    <div>
+      <img src={JPLogo} alt="Juice Press Logo" width="20%" height="20%"></img>
       <nav id='nav1'>
         <button>Profile</button>
         <button>Slack</button>
-        <button>Store Announcement</button>
+        <button>Create Announcement</button>
         <button>Google Workspace</button>
         <button>Blanket Pro</button>
       </nav>
 
       <div id='container2'>
         <h2>Important Announcements</h2>
-        <div className="p-3 bg-info my-2 rounded">
-          <toast>
-            <toastheader>
-              Reactstrap
-            </toastheader>
-            <toastbody>
-              This is a toast on a white background — check it out!
-            </toastbody>
-          </toast>
-        </div>
-        <div className="p-3 bg-warning my-2 rounded">
-          <toast>
-            <toastheader>
-              Reactstrap
-            </toastheader>
-            <toastbody>
-              This is a toast on a gridded background — check it out!
-            </toastbody>
-          </toast>
-        </div>
-        <div className="p-3 bg-info my-2 rounded">
-          <toast>
-            <toastheader>
-              Reactstrap
-            </toastheader>
-            <toastbody>
-              This is a toast on a primary background — check it out!
-            </toastbody>
-          </toast>
-        </div>
-      </div>
+       </div>
 
     <div id='conatiner3'>
       <button>Help</button>
       <button>Sign Out</button>
       </div>
-
     </div>
-
-
   )
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
 import './styles/Home.css';
+import JPLogo from './images/JPLogo.png';
 
 export default function Home() {
     return (
-        <div>
-            
+        <div id='container'>
+
+<img src={JPLogo} alt="Juice Press Logo" width="30%" height="30%"></img>
+      <nav id='nav1'>
+        <button>Profile</button>
+        <button>Slack</button>
+        <button>Store Announcement</button>
+        <button>Google Workspace</button>
+        <button>Blanket Pro</button>
+      </nav>
+
+      <div id='container2'>
+        <h2>Important Announcements</h2>
+        <div className="p-3 bg-info my-2 rounded">
+          <toast>
+            <toastheader>
+              Reactstrap
+            </toastheader>
+            <toastbody>
+              This is a toast on a white background — check it out!
+            </toastbody>
+          </toast>
         </div>
+        <div className="p-3 bg-warning my-2 rounded">
+          <toast>
+            <toastheader>
+              Reactstrap
+            </toastheader>
+            <toastbody>
+              This is a toast on a gridded background — check it out!
+            </toastbody>
+          </toast>
+        </div>
+        <div className="p-3 bg-info my-2 rounded">
+          <toast>
+            <toastheader>
+              Reactstrap
+            </toastheader>
+            <toastbody>
+              This is a toast on a primary background — check it out!
+            </toastbody>
+          </toast>
+        </div>
+      </div>
+
+    <div id='conatiner3'>
+      <button>Help</button>
+      <button>Sign Out</button>
+      </div>
+
+    </div>
     )
 }

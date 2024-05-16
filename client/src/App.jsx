f5348b8c9d86bd5ace75f6e133db6e35eead7f1b
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-05-16 19:37:37 -0400

    added routes to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-import './components/styles/Home.css';
-import JPLogo from './images/JPLogo.png';
-
-
-
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import LogIn from '.components/LogIn';
+import Home from '.components/Home';
 
 function App() {
-
-
+  const [token, setToken] = useState(localStorage.getItem("jwt-token"));
 
   return (
-    <div>
-      Test Page
-    </div>
-  )
- 
-    
+    <>
+    <Routes>
+    <Route path='/' element={<SignUp setToken={setToken}/>}></Route>
+    <Route path='/login' element={<LogIn setToken={setToken}/>}></Route>
+    <Route path='/home' element={<Home token={token}/>}></Route>
+    </Routes>
+    </>
+  ) 
 }
 
 export default App

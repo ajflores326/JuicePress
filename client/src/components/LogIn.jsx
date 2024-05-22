75e563a24b739ae63de192340bb99e6147f1708e
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-05-22 19:32:53 -0400

    adding admin sign up buttonto signup and log in page

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -42,7 +42,10 @@ export default function LogIn() {
     <button className="block bg-transparent hover:bg-green-400 text-green-400 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded" type = "Submit">Log In</button>
     <p>Don't have an account?</p>
     <a href = "/signup" className="bg-transparent hover:bg-green-400 text-green-400 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded"> Sign Up</a>
+    <p>Have a Juice Press Email?</p>
+    <a href = "" className="bg-transparent hover:bg-green-400 text-green-400 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded">Click Here</a>
     <img src={JPLogo} alt="Juice Press Logo" width="30%" height="30%"></img>
+    
     </form>
   )
 }
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -17,6 +17,8 @@ export default function SignUp() {
           <button className="block bg-transparent hover:bg-green-400 text-green-400 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded">Sign Up</button>
           <p>Already have an account?</p>
           <a href = "/" className="bg-transparent hover:bg-green-400 text-green-400 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded">Log In</a>
+          <p>Have a Juice Press Email?</p>
+          <a href = "" className="bg-transparent hover:bg-green-400 text-green-400 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded">Click Here</a>
 
 
           <img src={JPLogo} alt="Juice Press Logo" width="30%" height="30%"></img>

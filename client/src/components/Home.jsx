c6a1819821595e5f7b581f2081fc65edd2f221bb
Monisha Williams <mnwill333@gmail.com>
2024-06-04 19:44:10 -0400

    announcements stay on home pg after refresh

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,8 +7,8 @@ import Popup from 'reactjs-popup';
 import admin from '../../../server/models/admin';
 import user from "../../../server/models/user"
 import { useNavigate } from "react-router-dom";
-import {formatDistanceToNow, parseISO} from "date-fns"
-import DateDisplay from './DateDisplay'; 
+import { formatDistanceToNow, parseISO } from "date-fns"
+import DateDisplay from './DateDisplay';
 // import AllAnnouncements  from './AllAnnouncements'
 
 
@@ -29,17 +29,18 @@ export default function Home() {
   function navigateProfile() {
     navigate('/profile')
   }
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await (await fetch(`${import.meta.env.VITE_SERVER_URL}/`)).json()
-        setAnnouncements(response);
-        console.log(response)
+  //fetching all announcements from database
+  async function fetchData() {
+    try {
+      const response = await (await fetch(`${import.meta.env.VITE_SERVER_URL}/`)).json()
+      setAnnouncements(response);
+      console.log(response)
     } catch (error) {
-        console.error('Error fetching data:', error);
+      console.error('Error fetching data:', error);
     }
-}
+  }
+  useEffect(() => {
+
     fetchData();
   }, []);
 
@@ -48,8 +49,8 @@ export default function Home() {
     formData.append("title", announcementTitle);
     formData.append("content", announcementContent);
     formData.append("timestamp", new Date().toISOString())
-    if(announcementImage) formData.append("image", announcementImage);
-    if(announcementVideo) formData.append("video", announcementVideo);
+    if (announcementImage) formData.append("image", announcementImage);
+    if (announcementVideo) formData.append("video", announcementVideo);
 
 
     const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/createannouncement`, {
@@ -99,6 +100,7 @@ export default function Home() {
     setAnnouncementContent('');
     setAnnouncementImage(null);
     await createAnnouncement();
+    fetchData()
     document.getElementById('my_modal_2').close();
   };
 
@@ -109,7 +111,7 @@ export default function Home() {
       method: "GET",
       headers: {
         "authorization": localStorage.getItem("jwt-token")
-        
+
       },
 
     });
@@ -136,7 +138,7 @@ export default function Home() {
       method: "GET",
       headers: {
         "authorization": localStorage.getItem("jwt-tokenAdmin")
-        
+
       },
 
     });
@@ -164,22 +166,22 @@ export default function Home() {
     <div>
       <div>
         <div className='flex-row px-7 m-3 py-3'>
-          <img src={JPLogo} style={{ position: 'fixed', left: 40, top: '10%',  transform: 'translateY(-50%)' }} alt="Juice Press Logo" width="10%" height="10%"></img>
+          <img src={JPLogo} style={{ position: 'fixed', left: 40, top: '10%', transform: 'translateY(-50%)' }} alt="Juice Press Logo" width="10%" height="10%"></img>
         </div>
-        
+
         {token ?
-        <h1 className="flex justify-center font-bold text-2xl text-violet-600 tracking-wider  ">"Welcome {admin.firstName} {admin.lastName}!"</h1>
-        :<h1 className="flex justify-center font-bold text-2xl text-violet-600 tracking-wider ">"Welcome {user.firstName} {user.lastName}!"</h1>}
-       
-       <div>
-       <p className='flex justify-center text-xl font-semibold my-3'><DateDisplay /> </p>
-       </div>
-       
+          <h1 className="flex justify-center font-bold text-2xl text-violet-600 tracking-wider  ">"Welcome {admin.firstName} {admin.lastName}!"</h1>
+          : <h1 className="flex justify-center font-bold text-2xl text-violet-600 tracking-wider ">"Welcome {user.firstName} {user.lastName}!"</h1>}
+
+        <div>
+          <p className='flex justify-center text-xl font-semibold my-3'><DateDisplay /> </p>
+        </div>
+
         <div className='flex justify-center text-4xl'>
           <h2 className='block font-bold'>Important Announcements</h2>
         </div>
 
-         
+
         {/* <AllAnnouncements> </AllAnnouncements> */}
 
         <div className='flex flex-col items-center announcements p-4 border-secondary'>
@@ -204,56 +206,56 @@ export default function Home() {
           ))}
         </div>
 
-        
+
         <div className="content relative">
-        <nav className='nav1 m-16 font-semibold space-y-7' style={{ position: 'fixed', left: 0, top: '50%', transform: 'translateY(-50%)' }}>
-            <button onClick={()=> navigateProfile()} className='block btn rounded-full bg-primary hover:bg-secondary'>Profile</button>
+          <nav className='nav1 m-16 font-semibold space-y-7' style={{ position: 'fixed', left: 0, top: '50%', transform: 'translateY(-50%)' }}>
+            <button onClick={() => navigateProfile()} className='block btn rounded-full bg-primary hover:bg-secondary'>Profile</button>
             <button className='block btn rounded-full bg-primary hover:bg-secondary'>Slack</button>
             <button className='block btn rounded-full bg-primary hover:bg-secondary'>Help</button>
             <SignOut></SignOut>
             {token ?
-            <>
-                <button className='block btn bg-primary rounded-full hover:bg-secondary' onClick={()=>document.getElementById('my_modal_2').showModal()}> Create Post </button>
-                <dialog className='modal-box' id = "my_modal_2">
-                <form onSubmit={handleAnnouncementSubmit}>
+              <>
+                <button className='block btn bg-primary rounded-full hover:bg-secondary' onClick={() => document.getElementById('my_modal_2').showModal()}> Create Post </button>
+                <dialog className='modal-box' id="my_modal_2">
+                  <form onSubmit={handleAnnouncementSubmit}>
 
-                  <input
-                    className='rounded py-2 px-4 border border-black m-2'
-                    placeholder='Announcement Title'
-                    value={announcementTitle}
-                    onChange={(e) => setAnnouncementTitle(e.target.value)}
-                    required
-                  />
-                  <textarea
-                    className='rounded py-2 px-4 border border-black m-2'
-                    placeholder='Announcement Content'
-                    value={announcementContent}
-                    onChange={(e) => setAnnouncementContent(e.target.value)}
-                    required
-                  />
-                  <p>Image</p>
+                    <input
+                      className='rounded py-2 px-4 border border-black m-2'
+                      placeholder='Announcement Title'
+                      value={announcementTitle}
+                      onChange={(e) => setAnnouncementTitle(e.target.value)}
+                      required
+                    />
+                    <textarea
+                      className='rounded py-2 px-4 border border-black m-2'
+                      placeholder='Announcement Content'
+                      value={announcementContent}
+                      onChange={(e) => setAnnouncementContent(e.target.value)}
+                      required
+                    />
+                    <p>Image</p>
                     <input
                       className='block rounded py-2 px-4 border border-black m-2'
                       type='file'
                       accept='image/*'
                       onChange={(e) => setAnnouncementImage(e.target.files[0])}
-                     />
-                  <p>Video</p>
-                     <input
+                    />
+                    <p>Video</p>
+                    <input
                       className='block rounded py-2 px-4 border border-black m-2'
                       type='file'
                       accept='video/*'
                       onChange={(e) => setAnnouncementVideo(e.target.files[0])}
-                     />
-               <button className='btn bg-primary rounded-full px-9 py-3 hover:bg-secondary font-semibold' type='submit'>Submit</button>
+                    />
+                    <button className='btn bg-primary rounded-full px-9 py-3 hover:bg-secondary font-semibold' type='submit'>Submit</button>
                   </form>
                   <button className='btn bg-secondary rounded-full px-9 py-3 hover:bg-primary font-semibold' onClick={() => document.getElementById('my_modal_2').close()}>Close</button>
-                  </dialog>
+                </dialog>
               </>
               : ""}
           </nav>
 
-          
+
 
         </div>
       </div>

f037145937084c0c9665801e57304fd02eac7beb
Your Name <130116701+royfrias@users.noreply.github.com>
2024-06-06 01:33:27 -0400

    changes to app.js, routes.js, announcements.js, Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -29,47 +29,51 @@ export default function Home() {
   function navigateProfile() {
     navigate('/profile')
   }
+
   //fetching all announcements from database
   async function fetchData() {
     try {
-      const response = await (await fetch(`${import.meta.env.VITE_SERVER_URL}/`)).json()
-      setAnnouncements(response);
-      console.log(response)
+      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/announcements`,{
+        headers: {
+          authorization: localStorage.getItem('jwt-token')
+        }
+      });
+      if(!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setAnnouncements(data)
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.log("error fetching data:", error)
     }
-  }
-  useEffect(() => {
-
+  } useEffect(() => {
     fetchData();
   }, []);
 
+  // creates announcements and appends all of the main properties 
+  // announcementTitle, announcementTitle,timestamp, video, image
   async function createAnnouncement() {
     const formData = new FormData;
-    formData.append("title", announcementTitle);
-    formData.append("content", announcementContent);
+    formData.append("announcementTitle", announcementTitle);
+    formData.append("announcementContent", announcementTitle);
     formData.append("timestamp", new Date().toISOString())
     if (announcementImage) formData.append("image", announcementImage);
     if (announcementVideo) formData.append("video", announcementVideo);
 
 
-    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/createannouncement`, {
+  const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/createannouncement`, {
       method: "POST",
       headers: {
-        "content-type": "application/json",
         authorization: localStorage.getItem("jwt-token")
       },
-      body: JSON.stringify({
-        announcementTitle,
-        announcementContent,
-        formData
-      })
+      body: formData
     });
 
     if (response.status === 200) {
       const body = await response.json();
       alert(`Your announcement has been saved`);
-      return body;
+      fetchData();
+      return body.announcement;
     } else {
       const body = await response.json();
       console.log(body.message);
@@ -77,7 +81,7 @@ export default function Home() {
     }
   }
 
-  const handleCreateAnnouncement = () => {
+   const handleCreateAnnouncement = () => {
     setAnnouncementTitle('');
     setAnnouncementContent('');
     setAnnouncementImage(null);
@@ -87,22 +91,10 @@ export default function Home() {
 
   const handleAnnouncementSubmit = async (e) => {
     e.preventDefault();
-    const newAnnouncement = {
-      title: announcementTitle,
-      content: announcementContent,
-      image: announcementImage ? URL.createObjectURL(announcementImage) : null,
-      video: announcementVideo ? URL.createObjectURL(announcementVideo) : null,
-      timestamp: new Date().toISOString()
-    };
-
-    setAnnouncements([...announcements, newAnnouncement]);
-    setAnnouncementTitle('');
-    setAnnouncementContent('');
-    setAnnouncementImage(null);
     await createAnnouncement();
-    fetchData()
     document.getElementById('my_modal_2').close();
   };
+
 
   async function getUsername() {
 
@@ -110,10 +102,8 @@ export default function Home() {
     const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/user/username`, {
       method: "GET",
       headers: {
-        "authorization": localStorage.getItem("jwt-token")
-
+        authorization: localStorage.getItem("jwt-token")
       },
-
     });
 
     //getting user object
@@ -125,7 +115,6 @@ export default function Home() {
       console.log("error");
     }
   }
-
   //once user is logged in first and last name of user will be displayed on home pg
   useEffect(() => {
     getUsername()
@@ -137,8 +126,7 @@ export default function Home() {
     const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/admin/adminUsername`, {
       method: "GET",
       headers: {
-        "authorization": localStorage.getItem("jwt-tokenAdmin")
-
+        authorization: localStorage.getItem("jwt-tokenAdmin")
       },
 
     });
@@ -162,19 +150,19 @@ export default function Home() {
 
   // const [hasRender, setRender] = useState(false);
 
-  return (
+   return (
     <div>
       <div>
-        <div className='flex-row px-7 m-3 py-3'>
-          <img src={JPLogo} style={{ position: 'fixed', left: 40, top: '10%',  transform: 'translateY(-50%)' }} alt="Juice Press Logo" width="10%" height="10%"></img>
+        <div className='flex-row px-7 m-2 py-3'>
+          <img src={JPLogo} style={{ position: 'fixed', left: 40, top: '10%', transform: 'translateY(-50%)' }} alt="Juice Press Logo" width="10%" height="10%"></img>
         </div>
         
         {token ?
-        <h1 className="flex justify-center font-bold text-2xl mt-14">"Welcome {admin.firstName} {admin.lastName}!"</h1>
-        :<h1 className="flex justify-center font-bold text-2xl mt-14">"Welcome {user.firstName} {user.lastName}!"</h1>}
+          <h1 className="flex justify-center font-bold text-2xl mt-3">"Welcome {admin.firstName} {admin.lastName}!"</h1>
+          : <h1 className="flex justify-center font-bold text-2xl mt-5">"Welcome {user.firstName} {user.lastName}!"</h1>}
        
        <div>
-       <p className='flex justify-center text-2xl font-bold py-4'><DateDisplay /> </p>
+        <p className='flex justify-center text-2xl font-bold py-2'><DateDisplay /> </p>
        </div>
        
         <div className='flex justify-center text-4xl'>
@@ -182,25 +170,23 @@ export default function Home() {
         </div>
 
 
-        {/* <AllAnnouncements> </AllAnnouncements> */}
-
         <div className='flex flex-col items-center announcements p-4 border-secondary'>
           {announcements.map((announcement, index) => (
             <div key={index} className="card lg:card-side bg-base-100 shadow-xl m-4 w-1/2">
               {announcement.image && (
                 <figure className="flex-shrink-0 w-1/3">
-                  <img src={announcement.image} alt="Announcement" className="object-cover w-full h-48" />
+                  <img src={`${import.meta.env.VITE_SERVER_URL}/uploads/${announcement.image}`} alt="Announcement" className="object-cover w-full h-48" />
                 </figure>
               )}
               {announcement.video && (
                 <figure className="flex-shrink-0 w-1/3">
-                  <video controls src={announcement.video} className="object-cover w-full h-48"></video>
+                  <video controls src={`${import.meta.env.VITE_SERVER_URL}/uploads/${announcement.video}`} className="object-cover w-full h-48"></video>
                 </figure>
               )}
               <div className="card-body">
                 <h3 className='font-bold text-xl'>{announcement.announcementTitle}</h3>
                 <p>{announcement.announcementContent}</p>
-                {announcement.timestamp && <p>{formatDistanceToNow(parseISO(announcement.timestamp))} ago</p>} {/* Display timestamp */}
+                {announcement.timestamp && <p>{formatDistanceToNow(parseISO(announcement.timestamp))} ago</p>}
               </div>
             </div>
           ))}
@@ -208,68 +194,62 @@ export default function Home() {
 
         {/* nav bar */}
         <div className="content relative">
-        <nav className='flex flex-col nav1 font-semibold bg-accent py-80 space-y-12' style={{ position: 'fixed', left: 0, top: '50%', transform: 'translateY(-50%)' }}>
+          <nav className='flex flex-col nav1 font-semibold bg-accent py-80 space-y-12' style={{ position: 'fixed', left: 0, top: '50%', transform: 'translateY(-50%)' }}>
 
-        <div className='flex-row'>
-          <img src={JPLogo} style={{ position: 'fixed', left: 17, top: '23%',  transform: 'translateY(-50%)' }} alt="Juice Press Logo" width="85%" height="85%"></img>
-        </div>
+            <div className='flex-row'>
+              <img src={JPLogo} style={{ position: 'fixed', left: 17, top: '23%', transform: 'translateY(-50%)' }} alt="Juice Press Logo" width="85%" height="85%"></img>
+            </div>
 
-                <div className='flex flex-col space-y-9'>
-            <button onClick={()=> navigateProfile()} className='block btn rounded-full bg-primary hover:bg-secondary outline outline-offset-1 outline-black w-36 mx-14'>Profile</button>
-            <button className='block btn rounded-full bg-primary hover:bg-secondary outline outline-offset-1 outline-black w-26 mx-14'>Slack</button>
-            <SignOut></SignOut>
-            {token ?
-            <>
-                <button className='block btn bg-primary rounded-full hover:bg-secondary outline outline-offset-1 outline-black w-42 mx-14' onClick={()=>document.getElementById('my_modal_2').showModal()}> Create Post </button>
-                <dialog className='modal-box' id = "my_modal_2">
-                <form onSubmit={handleAnnouncementSubmit}>
+            <div className='flex flex-col space-y-9'>
+              <button onClick={() => navigateProfile()} className='block btn rounded-full bg-primary hover:bg-secondary outline outline-offset-1 outline-black w-36 mx-14'>Profile</button>
+              <button className='block btn rounded-full bg-primary hover:bg-secondary outline outline-offset-1 outline-black w-26 mx-14'>Slack</button>
+              <SignOut></SignOut>
+              {token ?
+                <>
+                  <button className='block btn bg-primary rounded-full hover:bg-secondary outline outline-offset-1 outline-black w-42 mx-14' onClick={handleCreateAnnouncement}> Create Post </button>
+                  <dialog className='modal-box' id="my_modal_2">
+                    <form onSubmit={handleAnnouncementSubmit}>
+                      <label className='block'>Title:</label>
+                      <input
+                        className='rounded py-2 px-4 border border-black m-2 w-96'
+                        placeholder='Announcement Title'
+                        value={announcementTitle}
+                        onChange={(e) => setAnnouncementTitle(e.target.value)}
+                        required
+                      />
 
-                    <label className='block'>Title:</label>
-                    <input
-                      className='rounded py-2 px-4 border border-black m-2 w-96'
-                      placeholder='Announcement Title'
-                      value={announcementTitle}
-                      onChange={(e) => setAnnouncementTitle(e.target.value)}
-                      required
-                    />
-
-                    <label className='block'>Content:</label>
-                    <textarea
-                      className='rounded py-2 px-4 border border-black m-2 w-96 h-80'
-                      placeholder='Announcement Content'
-                      value={announcementContent}
-                      onChange={(e) => setAnnouncementContent(e.target.value)}
-                      required
-                    />
-                    <p>Upload Image:</p>
-                    <input
-                      className='block rounded py-2 px-4 border border-black m-2'
-                      type='file'
-                      accept='image/*'
-                      onChange={(e) => setAnnouncementImage(e.target.files[0])}
-                    />
-                    <p>Upload Video:</p>
-                    <input
-                      className='block rounded py-2 px-4 border border-black m-2'
-                      type='file'
-                      accept='video/*'
-                      onChange={(e) => setAnnouncementVideo(e.target.files[0])}
-                    />
-                    <button className='btn bg-primary rounded-full px-7 py-3 hover:bg-secondary font-semibold mt-3 ml-44' type='submit'>Submit</button>
-                  </form>
-                  <button className='btn bg-secondary rounded-full px-6 py-3 hover:bg-primary font-semibold mt-3 ml-96' onClick={() => document.getElementById('my_modal_2').close()}>Close</button>
-                </dialog>
-              </>
-              : ""}
-              </div>
+                      <label className='block'>Content:</label>
+                      <textarea
+                        className='rounded py-2 px-4 border border-black m-2 w-96 h-80'
+                        placeholder='Announcement Content'
+                        value={announcementContent}
+                        onChange={(e) => setAnnouncementContent(e.target.value)}
+                        required
+                      />
+                      <p>Upload Image:</p>
+                      <input
+                        className='block rounded py-2 px-4 border border-black m-2'
+                        type='file'
+                        accept='image/*'
+                        onChange={(e) => setAnnouncementImage(e.target.files[0])}
+                      />
+                      <p>Upload Video:</p>
+                      <input
+                        className='block rounded py-2 px-4 border border-black m-2'
+                        type='file'
+                        accept='video/*'
+                        onChange={(e) => setAnnouncementVideo(e.target.files[0])}
+                      />
+                      <button className='btn bg-primary rounded-full px-7 py-3 hover:bg-secondary font-semibold mt-3 ml-33' type='submit'>Submit</button>
+                    </form>
+                    <button className='btn bg-secondary rounded-full px-6 py-3 hover:bg-primary font-semibold mt-3 ml-33' onClick={() => document.getElementById('my_modal_2').close()}>Close</button>
+                  </dialog>
+                </>
+                : ""}
+            </div>
           </nav>
-          
-
-
         </div>
       </div>
-
     </div>
-
   );
 }
--- server/models/announcements.js
+++ server/models/announcements.js
@@ -1,19 +1,29 @@
 import { Schema, model } from "mongoose";
 
 const announcementSchema = new Schema({
-    date: {
-        type: Date,
-        default: Date.now,
-    },
-    user: {
-        type: String,
-    },
-    announcementTitle: {
-        type: String,
-    },
-    announcementContent: {
-        type: String,
-    }
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: String,
+  },
+  announcementTitle: {
+    type: String,
+  },
+  announcementContent: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  video: {
+    type: String,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default model("Announcement", announcementSchema);
